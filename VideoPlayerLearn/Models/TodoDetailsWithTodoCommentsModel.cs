430a018fac721d48cbbeac192d0eb5cca822ef35
Melih Ömer Kamar <melih.o.k@hotmail.com>
2024-03-27 20:53:26 +0300

    Bildirimi gören kullanıcılar işlemlerini ve bildirim detayına gidildiğinde notfylerin görüldü işaretlenmesi işlemlerini actionfilterattribute ile yapılması için refactor edildi.

--- VideoPlayerLearn/Models/TodoDetailsWithTodoCommentsModel.cs
+++ VideoPlayerLearn/Models/TodoDetailsWithTodoCommentsModel.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
 using VideoPlayerLearn.Business.Dtos;
 using VideoPlayerLearn.Entities;
-using VideoPlayerLearn.Entities.Dtos;
 using VideoPlayerLearn.Entities.Dtos.TodoDtos;
 
 namespace VideoPlayerLearn.Models
@@ -10,9 +9,6 @@ namespace VideoPlayerLearn.Models
     {
  
         public Todo Todo { get; set; }
-        //public List<TodoComment> TodoComments { get; set; }
-        //public TodoCommentCreateDto TodoCommentCreate { get; set; }
-        public List<TodoFile> TodoFiles { get ; set; }
         public TodoResolutionDto ResolutionDto{ get; set; }
         public TodoReviewDto ReviewDto { get; set; }
         public TodoAssignDto TodoAssignDto { get; set; }
